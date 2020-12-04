320e1868b9a38eae28bbc04da10a35d3039f8971
Piotr Kamiński <kaminsky.pyetrek@gmail.com>
2020-12-04 17:54:48 +0100

    Added Topic and Comment entities

--- src/main/java/com/forum/forum_backend/services/CommentServiceImpl.java
+++ src/main/java/com/forum/forum_backend/services/CommentServiceImpl.java
@@ -1,16 +1,11 @@
 package com.forum.forum_backend.services;
 
-import com.forum.forum_backend.repositories.CommentRepository;
 import com.forum.forum_backend.services.interfaces.CommentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
 public class CommentServiceImpl implements CommentService {
-
-	@Autowired
-	private CommentRepository commentRepository;
 
 }
