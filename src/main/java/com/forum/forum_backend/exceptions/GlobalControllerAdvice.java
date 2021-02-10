a29d5e50b97f360bf382fe9da87d3b6d02344f78
Piotr Kamiński <kaminsky.pyetrek@gmail.com>
2021-02-10 14:58:01 +0100

    Implemented error response body as DTO

--- src/main/java/com/forum/forum_backend/exceptions/GlobalControllerAdvice.java
+++ src/main/java/com/forum/forum_backend/exceptions/GlobalControllerAdvice.java
@@ -1,34 +1,44 @@
 package com.forum.forum_backend.exceptions;
 
+import com.forum.forum_backend.dtos.ExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
 	@ExceptionHandler(UnauthorizedException.class)
-	public ResponseEntity<?> handleUnauthorizedException (UnauthorizedException ex) {
-		return new ResponseEntity<>(ex.getMessage(), ex.getHttpStatus());
+	public ResponseEntity<ExceptionDto> handleUnauthorizedException (UnauthorizedException ex, HttpServletRequest request) {
+		ExceptionDto exception = new ExceptionDto();
+		exception.setStatus(ex.getHttpStatus().value());
+		exception.setError(ex.getHttpStatus().toString());
+		exception.setMessage(ex.getMessage());
+		exception.setPath(request.getRequestURI());
+		return new ResponseEntity<>(exception, ex.getHttpStatus());
 	}
 
 	@ExceptionHandler(NotFoundException.class)
-	public ResponseEntity<?> handleNotFoundException(NotFoundException ex) {
-		return new ResponseEntity<>(ex.getMessage(), ex.getHttpStatus());
+	public ResponseEntity<ExceptionDto> handleNotFoundException(NotFoundException ex, HttpServletRequest request) {
+		ExceptionDto exception = new ExceptionDto();
+		exception.setStatus(ex.getHttpStatus().value());
+		exception.setError(ex.getHttpStatus().toString().split(" ")[1]);
+		exception.setMessage(ex.getMessage());
+		exception.setPath(request.getRequestURI());
+		return new ResponseEntity<>(exception, ex.getHttpStatus());
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-		List<String> errors = new ArrayList<>();
-		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-			errors.add(error.getDefaultMessage());
-		}
-		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	public ResponseEntity<ExceptionDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+		ExceptionDto exception = new ExceptionDto();
+		exception.setStatus(HttpStatus.BAD_REQUEST.value());
+		exception.setError(HttpStatus.BAD_REQUEST.toString());
+		exception.setMessage(ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
+		exception.setPath(request.getRequestURI());
+		return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
 	}
 }
